f418ec61a4836544cc9c88986591c481c470cc1f
Matheus Nobre <matheusnobre56@gmail.com>
2025-02-07 23:42:47 +0000

    feat: category model

---
+++ src/category/infra/db/sequelize/__tests__/category.model.int-spec.ts
@@ -0,0 +1,76 @@
+import { DataType, Sequelize } from 'sequelize-typescript';
+import { CategoryModel } from '../category.model';
+import { Category } from '../../../../domain/category.entity';
+
+describe('CategoryModel Integration Tests', () => {
+  let sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      models: [CategoryModel],
+    });
+    await sequelize.sync({ force: true });
+  });
+
+  test('mapping props', () => {
+    const attributesMap = CategoryModel.getAttributes();
+    const attributes = Object.keys(CategoryModel.getAttributes());
+
+    expect(attributes).toStrictEqual([
+      'category_id',
+      'name',
+      'description',
+      'is_active',
+      'created_at',
+    ]);
+
+    expect(attributesMap.category_id).toMatchObject({
+      type: DataType.UUID(),
+      primaryKey: true,
+      field: 'category_id',
+      fieldName: 'category_id',
+    });
+
+    expect(attributesMap.name).toMatchObject({
+      type: DataType.STRING(255),
+      allowNull: false,
+      field: 'name',
+      fieldName: 'name',
+    });
+
+    expect(attributesMap.description).toMatchObject({
+      type: DataType.TEXT(),
+      allowNull: true,
+      field: 'description',
+      fieldName: 'description',
+    });
+
+    expect(attributesMap.is_active).toMatchObject({
+      type: DataType.BOOLEAN(),
+      allowNull: false,
+      field: 'is_active',
+      fieldName: 'is_active',
+    });
+
+    expect(attributesMap.created_at).toMatchObject({
+      type: DataType.DATE(3),
+      allowNull: false,
+      field: 'created_at',
+      fieldName: 'created_at',
+    });
+  });
+
+  test('should create a category', async () => {
+    const arrange = {
+      category_id: '123e4567-e89b-12d3-a456-426655440000',
+      name: 'test',
+      description: 'test',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const category = await CategoryModel.create(arrange);
+    expect(category.toJSON()).toStrictEqual(arrange);
+  });
+});
--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -7,8 +7,8 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>;
   bulkInsert(entities: E[]): Promise<void>;
   update(entity: E): Promise<void>;
-  delete(id: EntityId): Promise<void>;
-  findById(id: EntityId): Promise<E | null>;
+  delete(entity_id: EntityId): Promise<void>;
+  findById(entity_id: EntityId): Promise<E | null>;
   findAll(): Promise<E[]>;
   getEntity(): new (...args: any[]) => E;
 }
