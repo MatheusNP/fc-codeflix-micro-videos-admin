ee067e682a73b7c24f784ca913c966b9a03b4677
Matheus Nobre <matheusnobre56@gmail.com>
2025-02-14 02:33:57 +0000

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { ConfigModule } from './nest-modules/config-module/config.module';
 
 @Module({
-  imports: [DatabaseModule, CategoriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    CategoriesModule,
+  ],
 })
 export class AppModule {}
--- src/nest-modules/categories-module/categories.controller.spec.ts
+++ src/nest-modules/categories-module/categories.controller.spec.ts
@@ -1,21 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database-module/database.module';
 import { CategoriesModule } from './categories.module';
+import { ConfigModule } from '../config-module/config.module';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule, CategoriesModule],
+      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
-    console.log(controller);
     expect(controller).toBeDefined();
   });
 });
