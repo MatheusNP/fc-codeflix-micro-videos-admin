46050ec55c6ec85eeab0e2a24e22f1c3df24a9cf
Matheus Nobre <matheusnobre56@gmail.com>
2025-03-16 22:33:21 +0000

    feat: genre sequelize repository

--- src/core/shared/infra/testing/expect-helpers.ts
+++ src/core/shared/infra/testing/expect-helpers.ts
@@ -1,9 +1,10 @@
-import { Notification } from '../../domain/validators/notification';
+import { Notification } from '@core/shared/domain/validators/notification';
+import { ValueObject } from '@core/shared/domain/value-object';
 
 expect.extend({
   notificationContainsErrorMessages(
     expected: Notification,
-    received: Array<string | { [key: string]: string[] }>
+    received: Array<string | { [key: string]: string[] }>,
   ) {
     const every = received.every((error) => {
       if (typeof error === 'string') {
@@ -27,8 +28,19 @@ expect.extend({
           pass: false,
           message: () =>
             `The validation errors not contains ${JSON.stringify(
-              received
+              received,
             )}. Current: ${JSON.stringify(expected.toJSON())}`,
+        };
+  },
+  toBeValueObject(expected: ValueObject, received: ValueObject) {
+    return expected.equals(received)
+      ? { pass: true, message: () => '' }
+      : {
+          pass: false,
+          message: () =>
+            `The value objects are not equals. Expected: ${JSON.stringify(
+              expected,
+            )} | Received: ${JSON.stringify(received)}`,
         };
   },
 });
