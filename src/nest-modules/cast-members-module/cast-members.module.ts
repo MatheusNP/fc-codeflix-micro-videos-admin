7fcf1d9210072d1048f803712c4a5d1d955e370d
Matheus Nobre <matheusnobre56@gmail.com>
2025-06-02 01:45:42 +0000

    feat: video migrations

--- src/nest-modules/cast-members-module/cast-members.module.ts
+++ src/nest-modules/cast-members-module/cast-members.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { CastMembersController } from './cast-members.controller';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { CastMemberModel } from '@core/cast-member/infra/db/sequelize/cast-member.model';
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CastMembersController } from './cast-members.controller';
 import { CAST_MEMBERS_PROVIDERS } from './cast-members.providers';
 
 @Module({
@@ -10,6 +10,12 @@ import { CAST_MEMBERS_PROVIDERS } from './cast-members.providers';
   providers: [
     ...Object.values(CAST_MEMBERS_PROVIDERS.REPOSITORIES),
     ...Object.values(CAST_MEMBERS_PROVIDERS.USE_CASES),
+    ...Object.values(CAST_MEMBERS_PROVIDERS.VALIDATIONS),
+  ],
+  exports: [
+    CAST_MEMBERS_PROVIDERS.REPOSITORIES.CAST_MEMBER_REPOSITORY.provide,
+    CAST_MEMBERS_PROVIDERS.VALIDATIONS
+      .CAST_MEMBERS_IDS_EXISTS_IN_DATABASE_VALIDATOR.provide,
   ],
 })
 export class CastMembersModule {}
